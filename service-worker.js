0f8e22ccd7f372af1fc7fe807157bd013be13bf1
atomic-minds <atomicminds26@gmail.com>
2026-09-18 13:10:41 +0530

    Update CACHE_NAME to atomic-minds-v4

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
 // resilience. Firebase, Firestore's realtime channel, Google Drive,
 // YouTube, and MathJax's CDN are never touched by this file at all.
 
-const CACHE_NAME = 'atomic-minds-v3'; 
+const CACHE_NAME = 'atomic-minds-v4'; 
 const STATIC_ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
